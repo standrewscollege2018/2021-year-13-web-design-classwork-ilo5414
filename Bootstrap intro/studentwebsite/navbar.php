d187bcb3b5c463e98cbd7d79f628a0c5cb9948d4
ilo5414 <ilo5414@stacmail.net>
2021-02-24 13:34:46 +1300

    oshdf

--- Bootstrap intro/studentwebsite/navbar.php
+++ Bootstrap intro/studentwebsite/navbar.php
@@ -1,22 +1,62 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-$tutor_sql = "SELECT * FROM tutorgroup";
-$tutor_qry = mysqli_query($dbconnect, $tutor_sql);
-$tutor_aa = mysqli_fetch_assoc($tutor_qry);
- ?>
- <a href="index.php"><h1>St Andrew's College</h1></a>
- <h2>Tutor groups</h2>
- <?php
-   do {
-     $tutorgroupID = $tutor_aa['tutorgroupID'];
-     $tutorcode = $tutor_aa['tutorcode'];
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-     echo "<a href='index.php?page=tutorgroup&tutorgroupID=$tutorgroupID&tutorcode=$tutorcode'>$tutorcode</a>";
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <div class="col-md-8">
+          <a class="" href="index.php"><h1>St Andrew's College</h1></a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
 
-    } while ($tutor_aa = mysqli_fetch_assoc($tutor_qry))
- ?>
- <h2>Search for student</h2>
- <form class="" action="index.php?page=searchresults" method="post">
-   <input required type="text" name="search" placeholder="Student name">
-   <button type="submit" name="button">Search</button>
- </form>
+    <?php
+
+    $tutor_sql = "SELECT * FROM tutorgroup";
+    $tutor_qry = mysqli_query($dbconnect, $tutor_sql);
+    $tutor_aa = mysqli_fetch_assoc($tutor_qry);
+     ?>
+
+     <h2>Tutor groups</h2>
+     <?php
+       do {
+         $tutorgroupID = $tutor_aa['tutorgroupID'];
+         $tutorcode = $tutor_aa['tutorcode'];
+
+         echo "<a href='index.php?page=tutorgroup&tutorgroupID=$tutorgroupID&tutorcode=$tutorcode'>$tutorcode</a>";
+
+        } while ($tutor_aa = mysqli_fetch_assoc($tutor_qry))
+     ?>
+     <h2>Search for student</h2>
+     <form class="" action="index.php?page=searchresults" method="post">
+       <input required type="text" name="search" placeholder="Student name">
+       <button type="submit" name="button">Search</button>
+     </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
